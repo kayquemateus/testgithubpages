2f39aa36cb2e901d18cda49e7632fbc1822e23a7
Kayque Mateus Marques de Paiva Barreto <07kayque@gmail.com>
2026-09-16 22:21:30 -0300

    learning js

---
+++ script.js
@@ -0,0 +1,40 @@
+// var nomeUsuario = prompt('Digite seu nome:');
+
+// console.log('Nome do usuário inserio:', nomeUsuario);
+// // interação
+// // nao é interessante para o usuario
+
+// var titulo = document.getElementById('titulo')
+// // a variavel titulo nesse caso captura o conteudo do index
+
+// titulo.textContent = `Olá, ${nomeUsuario}!`
+
+// alert('Seja bem-vindo')
+// // so alerta
+
+
+// alert('welcome');
+// // var name = prompt('Type your name');
+// // alert('Writed name: ' + name);
+
+// var num = prompt('type an number');
+// var numConvertido = parseInt(num);
+// alert(numConvertido + 10);
+
+// parseInt
+// // converte para inteiro
+// parseFloats
+// // converte pra float
+// Number
+// // funciona como um construtor
+
+// var num2 = parseInt(prompt('type your name'));
+// alert(num2 + 15);
+
+// // prompt - texto com interação
+// // alert - sem interação
+
+var num1 = parseInt(prompt('digite o primeiro numero'));
+var num2 = parseInt(prompt('digite o segundo numero'));
+console.log('A soma dos números do usuário é: ' + (num1 + num2));
+alert('O produto dos números do usuário é: ' + (num1 * num2));
